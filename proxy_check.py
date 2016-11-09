3e68729d226025ae407f1ed9e4ba4a0c30b1bf09
wen wang <wangw469@gmail.com>
2016-11-09 10:07:19 +0800

    Update proxy_check.py

--- proxy_check.py
+++ proxy_check.py
@@ -1,17 +1,30 @@
+#!/usr/bin/python
+
 import socks
 import sys
+import shutil
 
-s = socks.socksocket() # Same API as socket.socket in the standard lib
 
-s.set_proxy(socks.SOCKS5, "192.168.0.165", 1080) # SOCKS4 and SOCKS5 use port 1080 by default
-# Can be treated identical to a regular socket object
 try:
+    s = socks.socksocket() # Same API as socket.socket in the standard lib
     s.settimeout(1.0)
-    s.connect(("www.google.com", 80))
+    s.connect(("www.baidu.com", 80))
     s.close()
 except IOError:
-    print "Connection error! (Check proxy)"
+    shutil.copy("/usr/local/www/nginx/index_fail_fail.html", "/usr/local/www/nginx/index.html")
     sys.exit(1)
 else:
-    print "All was fine"
-    sys.exit(0)
+    s = socks.socksocket() # Same API as socket.socket in the standard lib
+    s.settimeout(1.0)
+    s.set_proxy(socks.SOCKS5, "192.168.0.165", 1080) # SOCKS4 and SOCKS5 use port 1080 by default
+    # Can be treated identical to a regular socket object
+    try:
+        s.settimeout(1.0)
+        s.connect(("www.google.com", 80))
+        s.close()
+    except IOError:
+        shutil.copy("/usr/local/www/nginx/index_ok_fail.html", "/usr/local/www/nginx/index.html")
+        sys.exit(1)
+    else:
+        shutil.copy("/usr/local/www/nginx/index_ok_ok.html", "/usr/local/www/nginx/index.html")
+        sys.exit(0)
